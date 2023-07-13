e858d7315baa62b54073ba1815bc37a54817d42a
IrshadBagwan <irshadbagwan706@gmail.com>
2023-07-13 12:36:17 +0530

    storing token to local storage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./components/Store/auth-context";
+import { useContext } from "react";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
+  const authctx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        {!authctx.isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        
+          <Route path="/profile">
+            {authctx.isLoggedIn && <UserProfile />}
+            {!authctx.isLoggedIn && <Redirect to='/auth'/>}
+          </Route>
+        
+        <Route path="*">
+          <Redirect to="/auth"></Redirect>{" "}
         </Route>
       </Switch>
     </Layout>
